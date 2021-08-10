4ecdaa5de345f695c3cfefa088a1758827cafc44
Michael Yang <yang6626@umn.edu>
2021-08-09 23:07:06 -0500

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,25 +14,27 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null,
-      comments: null
+      dishes: DISHES
     };
   }
 
 
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId});
-    this.setState({comments: this.state.dishes[dishId].comments})
-  }
-
   render() {
+
+    const HomePage =() => {
+
+      return(
+        <Home/>
+        );
+    }
   return (
     <div>
       <Header />
-      <Menu dishes = {this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)}/>
-      <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}  
-      comments={this.state.comments}/>
+      <Switch>
+        <Route path='/home' component={HomePage}/>
+        <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+        <Redirect to='/home'/>
+      </Switch>
       <Footer/>
     </div>
   );
