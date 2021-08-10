e9364b07583f672a01e97309f373e914f1c3681f
Michael Yang <yang6626@umn.edu>
2021-08-10 12:24:03 -0500

    single page app 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Menu from './MenuComponent';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 
 
@@ -66,9 +67,20 @@ import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reac
 
 		return (
 			<div className="container">
+				<div className="row ">
+					<Breadcrumb>
+						<BreadcrumbItem><Link to={'/menu'}>Menu</Link></BreadcrumbItem>
+						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.dish.name}</h3>
+
+					</div>
+				</div>
+
 				<div className="row">
 					<div className="col-12 col-md-5 m-1">
-						<RenderDish dish={props.selectedDish}/>
+						<RenderDish dish={props.dish}/>
 					</div>
 					<div className="col-12 col-md-5 m-1">
 						<RenderComments comments={props.comments}/>
