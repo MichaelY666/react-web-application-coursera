97714eb3407875debbe0cb164d47401b47863b2b
Michael Yang <yang6626@umn.edu>
2021-12-05 15:23:15 -0800

    fetch post comment

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -2,15 +2,46 @@ import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (dishId, rating, author, comment) => ({
+export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
-    payload: {
-        author: author,
-        comment: comment,
+    payload: comment
+});
+
+export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+    const newComment = {
         dishId: dishId,
         rating: rating,
-    }
-});
+        author: author,
+        comment: comment
+    };
+    newComment.date = new Date().toISOString();
+    return fetch(baseUrl + 'comments', {
+        method: 'POST',
+        body: JSON.stringify(newComment),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+    .then(response => {
+        if (response.ok){
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText)
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(response => dispatch(addComment(response)))
+    .catch(error => console.log('Post comments', error.message));
+    
+};
 
 // Thunk lets us return a function instead of an action
 // This can be useful for more complex actions, such as in fetchDishes below
